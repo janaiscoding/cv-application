e5518954590dccaa850855eac51dba83a49ae9cb
janaiscoding <jana.istrate@gmail.com>
2023-04-05 19:34:36 +0300

    Add Education functional component

---
+++ src/components/Education.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Education = () => {
+  const [schoolName, setSchoolName] = useState("");
+  const [degreeTitle, setDegreeTitle] = useState("");
+  const [graduationDate, setGraduationDate] = useState("");
+
+  return (
+    <>
+      <fieldset className="education">
+        {/* separate input field visually : SCHOOL NAME  */}
+        <label>
+          School Name
+          <input
+            value={schoolName}
+            onChange={(e) => setSchoolName(e.target.value)}
+            placeholder="School Name"
+            required={true}
+          />
+        </label>
+        {/* separate input field visually : DEGREE TITLE  */}
+        <label>
+          Degree Title
+          <input
+            value={degreeTitle}
+            onChange={(e) => setDegreeTitle(e.target.value)}
+            placeholder="Degree Title"
+            required={true}
+          />
+        </label>
+        {/* separate input field visually : GRADUATION YEAR */}
+        <label>
+          Graduation Year
+          <input
+            value={graduationDate}
+            onChange={(e) => setGraduationDate(e.target.value)}
+            placeholder="YYYY"
+            pattern="[0-9]{4}"
+            type="number"
+            required={true}
+          />
+        </label>
+      </fieldset>
+      {schoolName && degreeTitle && graduationDate !== "" && (
+        <div className="output">
+          <p>
+            You studied at {schoolName} for the {degreeTitle} and you graduated
+            in the year of {graduationDate}.
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Education;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Personal from "./Personal";
 import Experience from "./Experience";
+import Education from "./Education";
 
 const Resume = () => {
   return (
     <>
-      <form>
+      <div className="main">
         <Personal />
         <Experience />
-      </form>
+        <button>Add</button>
+        <Education />
+        <button>Add</button>
+      </div>
     </>
   );
 };
